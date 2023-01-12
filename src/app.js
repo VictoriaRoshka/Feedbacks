10bb0ea4fe420be87f9eccbc6d099ecf63c4466e
VictoriaRoshka <kawaii0109@mail.ru>
2023-01-12 14:40:23 +0300

    загружены другие изображения для кнопок навигации

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ let feedbacks = [
     },
     {image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
     name:'Anna Johnson',
-job:'Anna Johnson',
+job:'WEB DESIGNER',
 text:"Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
 },
 {image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
