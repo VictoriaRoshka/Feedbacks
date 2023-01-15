26ae98fae63cae9cf0379ad9444331ac6da159cd
VictoriaRoshka <kawaii0109@mail.ru>
2023-01-15 18:44:43 +0300

    форматирование кода

--- src/app.js
+++ src/app.js
@@ -1,78 +1,73 @@
 let i = 0
 let feedbacks = [
-    {image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-    name:'Susan Smith',
-    job:'WEB DEVELOPER',
-    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-    },
-    {image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
-    name:'Anna Johnson',
-job:'WEB DESIGNER',
-text:"Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
-},
-{image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-name:'Peter Jones',
-job:'INTERN',
-text:"Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-},
-{image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
-name:'Bill Anderson',
-job:'THE BOSS',
-text:'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.',
-},
+  {
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "Susan Smith",
+    job: "WEB DEVELOPER",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+  },
+  {
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    name: "Anna Johnson",
+    job: "WEB DESIGNER",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
+  },
+  {
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    name: "Peter Jones",
+    job: "INTERN",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag."
+  },
+  {
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    name: "Bill Anderson",
+    job: "THE BOSS",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic."
+  }
 ]
 
-    const feedbackBox = document.getElementById("feedback-box")
-    const previousBtn = document.getElementById("previous-btn")
-    const nextBtn = document.getElementById("next-btn")
-    const surpriseBtn = document.getElementById("surprise-btn")
-    previousBtn.addEventListener('click', previousReview)
-    nextBtn.addEventListener('click',nextReview)
-    surpriseBtn.addEventListener('click', randomReview)
+const feedbackBox = document.getElementById("feedback-box")
+const previousBtn = document.getElementById("previous-btn")
+const nextBtn = document.getElementById("next-btn")
+const surpriseBtn = document.getElementById("surprise-btn")
+previousBtn.addEventListener("click", previousReview)
+nextBtn.addEventListener("click", nextReview)
+surpriseBtn.addEventListener("click", randomReview)
 
+function renderReview() {
+  let feedback = feedbacks[i]
 
-
-    function renderReview(){
-
-        let feedback = feedbacks[i]
-
-        feedbackBox.innerHTML =  `
+  feedbackBox.innerHTML = `
             <div ><img class="feedback-box__image" src="${feedback.image}" alt="" /></div>
             <div class="feedback-box__name">${feedback.name}</div>
             <div class="feedback-box__job">${feedback.job}</div>
             <div class="feedback-box__text">${feedback.text}</div>`
+}
+renderReview()
 
-    }
-    renderReview()
+function nextReview() {
+  i += 1
 
-    function nextReview(){
+  if (i === 4) {
+    i = 0
+  }
+  renderReview()
+}
+function previousReview() {
+  i -= 1
 
-        i+= 1
+  if (i === -1) {
+    i = 4
+  }
+  renderReview()
+}
 
-        if(i === 4){
-            i = 0
-        }
-        renderReview()
-        
-    }
-    function previousReview(){
-
-        i-= 1
-
-        if(i === -1){
-            i = 4
-        }
-        renderReview()
-    }
-
-    function randomReview(){
-        let randomIndex = Math.floor(Math.random() * feedbacks.length)
-        i = randomIndex
-        renderReview()
-    }
-
-
-    
-
-            
-
+function randomReview() {
+  let randomIndex = Math.floor(Math.random() * feedbacks.length)
+  i = randomIndex
+  renderReview()
+}
